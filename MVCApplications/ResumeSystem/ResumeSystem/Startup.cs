370f367da9142f939e3a05007f5930183bf859c4
HelloWare <deyikong@hellowareacademy.com>
2017-08-03 21:36:18 -0500

    added resume system

---
+++ MVCApplications/ResumeSystem/ResumeSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResumeSystem.Startup))]
+namespace ResumeSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
